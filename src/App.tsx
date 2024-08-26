81ddfd6c0522a6441bfa9b19e9dadcb4fd46b522
alamin <codepapa360@gmail.com>
2024-08-26 21:12:29 +0600

    style: Update layout in App component

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import RuleBtn from "./components/RuleBtn";
 
 function App() {
   return (
-    <div className="grid grid-rows-[1fr_auto] min-h-screen p-2 bg-gradient-to-b from-slate-800 to-slate-900 overflow-x-hidden">
+    <div className="grid grid-rows-[1fr_auto] min-h-dvh p-2 bg-gradient-to-b from-slate-800 to-slate-900 overflow-x-hidden">
       <main className="grid grid-rows-[auto_1fr_auto] px-2 py-4 overflow-hidden">
         <Header />
         <GameBoard />
